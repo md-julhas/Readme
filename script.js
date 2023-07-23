7dc661f17671cb1f1da9a80c4abe5fe7718e3c4d
Julhas <julhas.info@gmail.com>
2023-07-23 14:18:57 +0600

    clean

--- script.js
+++ script.js
@@ -2,18 +2,3 @@ const nameFn = () => {
   console.log("name Fn")
 }
 nameFn()
-
-// here is a comment
-
-const name = 10000
-
-const font = arial
-
-// New line
-const scriptjs = () => {
-  console.log(name)
-}
-scriptjs()
-
-
-const fn = "sting"
